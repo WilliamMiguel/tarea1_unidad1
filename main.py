25bd1c1ca5a22ad0ed7c15d377ab82452a86456a
WilliamMiguel <113279478+WilliamMiguel@users.noreply.github.com>
2022-11-03 12:33:52 -0500

    Reemplazo de función para mostrar libros

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from libro import *
 from tabulate import tabulate
 import os
 import csv
+os.system("pip install tabulate")
+
 
 def loadBooks():
     with open("books.csv", "r", encoding='utf-8') as f:
@@ -11,21 +13,6 @@ def loadBooks():
             books.append(row)
     return books
 
-# def listBooks(books):
-#     listofBooks = []
-#     for attribute in books:
-#         _id = attribute["id"]
-#         title = attribute["title"]
-#         genre = attribute["genre"]
-#         isbn = attribute["ISBN"]
-#         editorial = attribute["editorial"]
-#         authors = attribute["authors"].split(";")
-#         book = Libro(_id, title, genre, isbn, editorial, authors)
-#         listofBooks.append(book)
-
-#     for book in listofBooks:
-#         book.showBook()
-#         print()
 
 def tableBooks(books):
     ids = [book["id"] for book in books]
@@ -34,11 +21,11 @@ def tableBooks(books):
     isbn = [book["ISBN"] for book in books]
     editorial = [book["editorial"] for book in books]
     authors = [book["authors"] for book in books]
-
     tupleBooks = zip(ids, titles, genre, isbn, editorial, authors)
     fieldnames = ["ID", "Título", "Género", "ISBN", "Editorial", "Autor(es)"]
     print(tabulate(tupleBooks, headers=fieldnames))
     print()
+
 
 def isEmpty(texto):
     while True:
@@ -46,6 +33,7 @@ def isEmpty(texto):
         if len(attribute) != 0:
             break
     return attribute
+
 
 def option01():
     print()
@@ -63,22 +51,23 @@ def option01():
                 break
             books.append(row)
             bookNumber += 1
-        
+
     print("\nCARGANDO LIBROS...\n")
-    tableBooks(books)
     if bookNumber < int(upBooks):
         print(f"ENCONTRAMOS {bookNumber} LIBROS")
     else:
         print("CARGA COMPLETA")
+    return books
 
-def option02():
+
+def option02(books):
     print("\nCONTAMOS CON LOS SIGUIENTES LIBROS...\n")
-    books = loadBooks()
     tableBooks(books)
     print("Carga completa")
 
 
 def option03():
+    books = loadBooks()
     print("\nAGREGANDO UN LIBRO...\n")
     _id = isEmpty("Ingrese el ID: ")
     title = isEmpty("Ingrese el título: ")
@@ -89,22 +78,26 @@ def option03():
         "Ingrese el autor o los autores (separados mediante ;): ")
     book = Libro(_id, title, genre, ISBN, editorial, authors)
     book.registro()
-    with open("books.csv", "a", encoding="utf-8", newline="\n") as f_write:
-        fieldnames = ["id", "title", "genre", "ISBN", "editorial", "authors"]
-        register = csv.DictWriter(f_write, fieldnames=fieldnames)
-        register.writerow(book.get_book())
+    books.append(book.get_book())
+
+    # with open("books.csv", "a", encoding="utf-8", newline="\n") as f_write:
+    #     fieldnames = ["id", "title", "genre", "ISBN", "editorial", "authors"]
+    #     register = csv.DictWriter(f_write, fieldnames=fieldnames)
+    #     register.writerow(book.get_book())
     print("\nSe agregó un libro")
+    return books
 
 
-def option04():
+def option04(books):
     print("\nELIMINANDO UN LIBRO...\n")
-    with open("books.csv", "r", encoding='utf-8') as f:
-        file = csv.DictReader(f)
-        books = []
-        for row in file:
-            books.append(row)
+    # with open("books.csv", "r", encoding='utf-8') as f:
+    #     file = csv.DictReader(f)
+    #     books = []
+    #     for row in file:
+    #         books.append(row)
 
     idAvailable = [book["id"] for book in books]
+    print(idAvailable)
 
     while True:
         delete = input("Ingrese el ID del libro a borrar: ")
@@ -114,19 +107,36 @@ def option04():
         else:
             print("Ingrese un ID válido")
 
-    books.pop(index)
+    # deleteBook = books.pop(index)
+    # print(deleteBook)
 
+    # with open("books.csv", "w", encoding="utf-8", newline="\n") as f_write:
+    #     fieldnames = ["id", "title", "genre", "ISBN", "editorial", "authors"]
+    #     register = csv.DictWriter(f_write, fieldnames=fieldnames)
+    #     register.writeheader()
+    #     register.writerows(books[:])
+
+    print("\nSe eliminó el libro")
+    return index
+
+# books = option01()
+# prueba = option04(books)
+# print(prueba)
+# z = prueba.difference(books)
+# print(dir(prueba))
+
+def option10(books):
     with open("books.csv", "w", encoding="utf-8", newline="\n") as f_write:
         fieldnames = ["id", "title", "genre", "ISBN", "editorial", "authors"]
         register = csv.DictWriter(f_write, fieldnames=fieldnames)
         register.writeheader()
         register.writerows(books[:])
+    print("\nCAMBIOS GUARDADOS")
 
-    print("\nSe eliminó el libro")
 
-
-options = ["option01()", "option02()", "option03()", "option04()", "option05()",
-           "option06()", "option07()", "option08()", "option09()", "option10()"]
+# options = ["option01()", "option02(books)", "option03()", "option04()", "option05()",
+#            "option06()", "option07()", "option08()", "option09()", "option10()"]
+# op01 = "option01()"
 
 
 def showOptions():
@@ -147,7 +157,7 @@ def showOptions():
 optionsNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
 
-
+books = loadBooks()
 while True:
     os.system("cls")
     print("\t\tBIENVENIDO(A) A NUESTRA BIBLIOTECA\n")
@@ -155,8 +165,20 @@ while True:
     while True:
         option = input("Ingrese el número de una opción: ")
         if option in optionsNumber:
+            option = int(option)
             break
-    eval(options[int(option)-1])
+
+    if option == 1:
+        books = option01()
+    if option == 2:
+        option02(books)
+    if option == 3:
+        books = option03()
+    if option == 4:
+        books = option04(books)
+    if option == 10:
+        option10(books)
+    # eval(options[int(option)-1])
     print("----------------------------------------------")
     while True:
         proceed = input("¿Desea elegir otra opción? S/N: ").upper()
@@ -168,3 +190,5 @@ while True:
         continue
     else:
         break
+
+# print(books)

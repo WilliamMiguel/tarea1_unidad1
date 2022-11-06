c7e3ee3902240448ac2c7d40aed5c89d2ec4dfbc
vmarialuzm <vmarialuzm@gmail.com>
2022-11-06 17:23:59 -0500

    Pregunta 1 y 4 terminada

--- pregunta1y4.py
+++ pregunta1y4.py
@@ -4,17 +4,20 @@ import json
 import requests
 from tabulate import tabulate
 
-
-#Opción 1: Listar pokemons por generación. Se ingresa alguna generación (1, 2, 3, ..) y se listan todos los pokemon respectivos.
-
-lista_generaciones=[1,2,3,4,5,6,7,8]
-
 def pokemones_generacion(generacion):
     url_generacion="https://pokeapi.co/api/v2/generation/"
     resp=requests.get(url_generacion+str(generacion))
 
     data =resp.json()
     lista_pokemones=[generacion['name'].capitalize() for generacion in data['pokemon_species']]
+    return lista_pokemones
+
+def pokemones_habitat(habitat):
+    url_habitat="https://pokeapi.co/api/v2/pokemon-habitat/"
+    resp=requests.get(url_habitat+habitat)
+
+    data =resp.json()
+    lista_pokemones=[habitat['name'].capitalize() for habitat in data['pokemon_species']]
     return lista_pokemones
 
 def listar_pokemon(pokemons):
@@ -39,51 +42,67 @@ def listar_pokemon(pokemons):
         
     return lista_habilidades, image_url
 
-while True:
-    print(f"\n¿Qué categoría desea elegir?\n")
-    for generation in lista_generaciones:
-        print(f"    Generación: {generation}")
-    print()
+def option01():
+    while True:
+        lista_generaciones=[1,2,3,4,5,6,7,8]
+        print("\n¿Qué categoría desea elegir?\n")
+        for generation in lista_generaciones:
+            print(f"    Generación: {generation}")
+        print()
 
-    while True:  
-        pregunta1 = input("Ingresa una generación: ")
-        if pregunta1.isnumeric() and int(pregunta1) in lista_generaciones:
+        while True:  
+            pregunta1 = input("Ingresa una generación: ")
+            if pregunta1.isnumeric() and int(pregunta1) in lista_generaciones:
+                    break
+            
+        pokemones=pokemones_generacion(pregunta1)
+
+        print("\nBuscando pokemons...\n")
+        listaPokemons = listar_pokemon(pokemones)
+
+        tuplePokemons = zip(pokemones,listaPokemons[0],listaPokemons[1])
+        fieldnames = ["Pokemon","Habilidades","URL Imagen"]
+        print(tabulate(tuplePokemons, headers=fieldnames))
+        print()
+
+        while True:
+            anotherGeneration = input("¿Elegir otra generación? S/N: ").upper()
+            if anotherGeneration == "S" or anotherGeneration == "N":
                 break
         
-    pokemones=pokemones_generacion(pregunta1)
-
-    print("\nBuscando pokemons...\n")
-    listaPokemons = listar_pokemon(pokemones)
-
-    tuplePokemons = zip(pokemones,listaPokemons[0],listaPokemons[1])
-    fieldnames = ["Pokemon","Habilidades","URL Imagen"]
-    print(tabulate(tuplePokemons, headers=fieldnames))
-    print()
-
-    while True:
-        anotherGeneration = input("¿Elegir otra generación? S/N: ").upper()
-        if anotherGeneration == "S" or anotherGeneration == "N":
+        if anotherGeneration == "N":
             break
-    
-    if anotherGeneration == "N":
-        break
 
-#Opción 4: Listar pokemons por habitat. Se deben sugerir opciones a ingresar para interactuar.
+def option04():
+    while True:
+        lista_habitat=["cave","forest","grassland","mountain","rare","rough-terrain","sea","urban","waters-edge"]   
+        number_habitat=[]
+        print("\n¿Qué habitat desea elegir?\n")
+        for index,habitat in enumerate(lista_habitat,1):
+            print(f"        {index}: {habitat.capitalize()}")
+            number_habitat.append(index)
+        print()
 
-lista_habitat=["cave","forest","grassland","mountain","rare","rough-terrain","sea","urban","waters-edge"]
+        while True:  
+                pregunta4 = input("Ingresa un habitat: ")
+                if pregunta4.isnumeric() and int(pregunta4) in number_habitat:
+                        break
+        
+        pokemones=pokemones_habitat(pregunta4)
 
-def pokemones_habitat(habitat):
-    url_habitat="https://pokeapi.co/api/v2/pokemon-habitat/"
-    resp=requests.get(url_habitat+habitat)
+        print("\nBuscando pokemons...\n")
+        listaPokemons = listar_pokemon(pokemones)
 
-    data =resp.json()
-    lista_pokemones=[habitat['name'] for habitat in data['pokemon_species']]
-    return lista_pokemones
-  
-while True:   
-    pregunta4=input(f"¿Qué habitat desea elegir? {lista_habitat}: ") 
-    if pregunta4 in lista_habitat:
-        break
-      
-pokemones=pokemones_habitat(pregunta4)
+        tuplePokemons = zip(pokemones,listaPokemons[0],listaPokemons[1])
+        fieldnames = ["Pokemon","Habilidades","URL Imagen"]
+        print(tabulate(tuplePokemons, headers=fieldnames))
+        print()
 
+        while True:
+            anotherHabitat = input("¿Elegir otro habitat? S/N: ").upper()
+            if anotherHabitat == "S" or anotherHabitat == "N":
+                break
+        
+        if anotherHabitat == "N":
+            break
+option01()
